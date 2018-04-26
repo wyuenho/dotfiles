23a7c9366d224f118d42350fa35c2f93ab647216
Jimmy Yuen Ho Wong <wyuenho@gmail.com>
2018-04-26 15:17:30 +0100

    Remove compiled snippets

--- .emacs.d/snippets/js2-mode/.yas-compiled-snippets.el
+++
@@ -1,2 +0,0 @@
-;;; Compiled snippets and support files for `js2-mode'
-;;; Do not edit! File generated at Thu Apr 26 04:40:03 2018
--- .emacs.d/snippets/rjsx-mode/.yas-compiled-snippets.el
+++
@@ -1,2 +0,0 @@
-;;; Compiled snippets and support files for `rjsx-mode'
-;;; Do not edit! File generated at Thu Apr 26 04:40:03 2018
