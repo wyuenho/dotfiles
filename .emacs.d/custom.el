1dae1b91af57a6434fadca02903e0eb92abb7770
Jimmy Yuen Ho Wong <wyuenho@gmail.com>
2018-05-27 00:34:50 +0100

    max width for flycheck pos tip

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -70,6 +70,7 @@
  '(flycheck-global-modes
    (quote
     (sh-mode json-mode yaml-mode js-mode js-jsx-mode js2-mode js2-jsx-mode rjsx-mode typescript-mode python-mode go-mode web-mode css-mode scss-mode rust-mode)))
+ '(flycheck-pos-tip-max-width 80)
  '(gc-cons-threshold 20000000)
  '(global-company-mode t)
  '(global-flycheck-mode t)
