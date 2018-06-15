57cd82977590ce2a83c755623e4140a2e0b86749
Jimmy Yuen Ho Wong <wyuenho@gmail.com>
2018-06-15 21:06:09 +0100

    give emacs half a gig of mem

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -86,7 +86,7 @@
    (quote
     (sh-mode json-mode yaml-mode js-mode js-jsx-mode js2-mode js2-jsx-mode rjsx-mode typescript-mode python-mode go-mode web-mode css-mode scss-mode rust-mode)))
  '(flycheck-pos-tip-max-width 80)
- '(gc-cons-threshold 20000000)
+ '(gc-cons-threshold 536870912)
  '(global-auto-revert-non-file-buffers t)
  '(global-company-mode t)
  '(global-diff-hl-mode t)
