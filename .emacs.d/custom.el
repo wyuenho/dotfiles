bc5e448e9114754e02ff449b29bd4308a84bdf47
Jimmy Yuen Ho Wong <wyuenho@gmail.com>
2018-06-18 01:37:41 +0100

    turn off flycheck for python

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -86,7 +86,7 @@
  '(flx-ido-use-faces t)
  '(flycheck-global-modes
    (quote
-    (sh-mode json-mode yaml-mode js-mode js-jsx-mode js2-mode js2-jsx-mode rjsx-mode typescript-mode python-mode go-mode web-mode css-mode scss-mode rust-mode)))
+    (sh-mode json-mode yaml-mode js-mode js-jsx-mode js2-mode js2-jsx-mode rjsx-mode typescript-mode go-mode web-mode css-mode scss-mode rust-mode)))
  '(flycheck-pos-tip-max-width 80)
  '(gc-cons-threshold 536870912)
  '(global-auto-revert-non-file-buffers t)
