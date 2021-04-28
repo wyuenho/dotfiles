a7bc548664160f8922924b9d61b853b39f0ff107
Jimmy Wong <wyuenho@gmail.com>
2021-04-29 00:56:10 +0100

    fix elisp vector indentation

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1151,6 +1151,22 @@ optionally the window if possible."
   :mode "\\.env\\..*\\'")
 
 ;; Emacs Lisp
+(defun calculate-lisp-indent-advice (fn &rest args)
+  "Don't indent vectors in `emacs-lisp-mode' like lists."
+  (if (save-excursion
+        (beginning-of-line)
+        (let ((start (point)))
+          (save-excursion
+            (beginning-of-defun)
+            (let ((containing-sexp (elt (parse-partial-sexp (point) start) 1)))
+              (when containing-sexp
+                (setf (point) containing-sexp)
+                (looking-at "\\["))))))
+      (let ((lisp-indent-offset 1))
+        (apply fn args))
+    (apply fn args)))
+(advice-add 'calculate-lisp-indent :around 'calculate-lisp-indent-advice)
+
 (pcase-dolist (`(,key . ,command)
                '(("C-c e f" . byte-compile-file)
                  ("C-c e c" . emacs-lisp-byte-compile)
