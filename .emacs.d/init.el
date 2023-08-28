0572cf763cb17b04278a74461dbe1fb1c85c26aa
Jimmy Yuen Ho Wong <wyuenho@gmail.com>
2023-08-29 00:55:48 +0100

    fix flycheck ruff checker formatting

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -937,26 +937,26 @@ checker symbol."
   :config
 
   (flycheck-define-checker python-ruff
-                           "A Python syntax and style checker using the ruff utility.
+    "A Python syntax and style checker using the ruff utility.
 To override the path to the ruff executable, set
 `flycheck-python-ruff-executable'.
 See URL `http://pypi.python.org/pypi/ruff'."
-                           :command ("ruff"
-                                     "--format=text"
-                                     (eval (when buffer-file-name
-                                             (concat "--stdin-filename=" buffer-file-name)))
-                                     "-")
-                           :standard-input t
-                           :error-filter (lambda (errors)
-                                           (let ((errors (flycheck-sanitize-errors errors)))
-                                             (seq-map #'flycheck-flake8-fix-error-level errors)))
-                           :error-patterns
-                           ((warning line-start
-                                     (file-name) ":" line ":" (optional column ":") " "
-                                     (id (one-or-more (any alpha)) (one-or-more digit)) " "
-                                     (message (one-or-more not-newline))
-                                     line-end))
-                           :modes python-mode)
+    :command ("ruff"
+              "--format=text"
+              (eval (when buffer-file-name
+                      (concat "--stdin-filename=" buffer-file-name)))
+              "-")
+    :standard-input t
+    :error-filter (lambda (errors)
+                    (let ((errors (flycheck-sanitize-errors errors)))
+                      (seq-map #'flycheck-flake8-fix-error-level errors)))
+    :error-patterns
+    ((warning line-start
+              (file-name) ":" line ":" (optional column ":") " "
+              (id (one-or-more (any alpha)) (one-or-more digit)) " "
+              (message (one-or-more not-newline))
+              line-end))
+    :modes python-mode)
 
   (add-to-list 'flycheck-checkers 'python-ruff)
 
