18030ad220bebbc8d0b0d2a3a4c71ae7238cdaa6
Jimmy Yuen Ho Wong <wyuenho@gmail.com>
2025-01-02 10:23:03 +0000

    remove emacs-lsp-booster

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -829,40 +829,6 @@ Optional argument ARG same as `comment-dwim''s."
 
   (setf read-process-output-max (* 1024 1024 10))
 
-  ;; https://github.com/blahgeek/emacs-lsp-booster
-  (defun lsp-booster--advice-json-parse (old-fn &rest args)
-    "Try to parse bytecode instead of json."
-    (or
-     (when (equal (following-char) ?#)
-       (let ((bytecode (read (current-buffer))))
-         (when (byte-code-function-p bytecode)
-           (funcall bytecode))))
-     (apply old-fn args)))
-
-  (advice-add (if (progn (require 'json)
-                         (fboundp 'json-parse-buffer))
-                  'json-parse-buffer
-                'json-read)
-              :around
-              #'lsp-booster--advice-json-parse)
-
-  (defun lsp-booster--advice-final-command (old-fn cmd &optional test?)
-    "Prepend emacs-lsp-booster command to lsp CMD."
-    (let ((orig-result (funcall old-fn cmd test?)))
-      (if (and (not test?)
-               (not (file-remote-p default-directory))
-               lsp-use-plists
-               (not (functionp 'json-rpc-connection))
-               (executable-find "emacs-lsp-booster"))
-          (progn
-            (when-let ((command-from-exec-path (executable-find (car orig-result))))
-              (setcar orig-result command-from-exec-path))
-            (message "Using emacs-lsp-booster for %s!" orig-result)
-            (cons "emacs-lsp-booster" orig-result))
-        orig-result)))
-
-  (advice-add 'lsp-resolve-final-command :around #'lsp-booster--advice-final-command)
-
   (with-eval-after-load 'flycheck
     (defvar-local lsp-flycheck-checkers nil)
 
